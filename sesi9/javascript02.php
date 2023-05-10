2ed136af826cd273e9d4f2b06a1af4ee7a9aee65
AnandaMahardikaa <127913994+AnandaMahardikaa@users.noreply.github.com>
2023-05-10 12:14:40 +0800

    Add files via upload

---
+++ sesi9/javascript02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan2</title>
+</head>
+<body>
+    <div id="pesan">Kita Isi</div>
+    <span id="pesan2"></span>
+    <script>
+        document.write("<P>BELAJAR MEMAHAMI</P>")
+        document.write("<strong>====================</strong>")
+        document.getElementById("pesan").innerHTML = "di ganti dengan apa <sup>saja boleh</sup>";
+        document.getElementById("pesan2").innerHTML = "apa aja <sup>BOLEH</sup>";
+    </script>
+
+</body>
+</html>
